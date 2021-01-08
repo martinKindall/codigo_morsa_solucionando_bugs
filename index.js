4e75ed7f4fd69f7042cc2fc36d25b18104b509cf
martinKindall <mart256@gmail.com>
2021-01-08 22:41:59 +0100

    Agregando bootstrap 4.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use('/', express.static('public'));
 
 app.listen(port, () => {
