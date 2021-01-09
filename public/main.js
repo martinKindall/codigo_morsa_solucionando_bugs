42915f329ca1f4e85d71dafb69306d5b599b1194
martinKindall <mart256@gmail.com>
2021-01-09 10:14:05 +0100

    Se agrega una funcion de prueba antes de mostrar el nombre

--- public/main.js
+++ public/main.js
@@ -2,9 +2,14 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     document.getElementById("btn-guardar")
         .onclick = (() => {
+            const resultado = unaFuncionDePrueba(2, 5);
             const input_nombre = document.getElementById("inp-nombre");
             const nombre = input_nombre.value;
             alert(`El nombre ${nombre} fue guardado!`);
             input_nombre.value = "";
         });
 });
+
+function unaFuncionDePrueba(num1, num2) {
+    return num1 * num2;
+}
